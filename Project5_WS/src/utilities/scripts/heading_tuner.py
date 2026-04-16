0d159dbdadf222c1532c82e399ef7ab4a83651d2
casey-harwood <casey-harwood@uiowa.edu>
2026-04-16 09:28:46 -0500

    Delete Project5_WS directory (weird case-sensitivity duplicate)

--- Project5_WS/src/utilities/scripts/heading_tuner.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from utilities.heading_tuner import main
-
-if __name__ == '__main__':
-    main()
--- Project5_WS/src/wskr/scripts/wskr_range.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from wskr.wskr_range_node import main
-
-
-if __name__ == '__main__':
-    main()
